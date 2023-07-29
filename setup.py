d7fc0f1d044dcd7b5166c9c996b2162e33b660df
De Vuyst Silas <silas.devuyst@ugent.be>
2023-07-29 20:28:44 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='strprogressbar',
+  version='0.0.1',
+  description='',
+  url='https://github.com/SDeVuyst/strprogressbar',  
+  author='SDeVuyst',
+  author_email='',
+  license='GNU General Public License v3 (GPLv3)', 
+  classifiers=classifiers,
+  keywords='String Progress Bar', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
---
+++ strprogressbar/__init__.py
@@ -0,0 +1,1 @@
+from strprogressbar import ProgressBar
